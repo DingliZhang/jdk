6a52ec04b0582b8f4219d6a3e9b7ab64e7a97de3
Dingli Zhang <dingli@iscas.ac.cn>
2022-04-20 23:08:43 +0800

    Fix bitset of push_reg/pop_reg in MacroAssembler::call_native_base
    Bitset of '0x80000040' here operates t1 & xmethod, it should be '0x80000020' for t0 & xmethod.

--- src/hotspot/cpu/riscv/macroAssembler_riscv.cpp
+++ src/hotspot/cpu/riscv/macroAssembler_riscv.cpp
@@ -580,13 +580,13 @@ void MacroAssembler::call_native(address entry_point, Register arg_0) {
 void MacroAssembler::call_native_base(address entry_point, Label *retaddr) {
   Label E, L;
   int32_t offset = 0;
-  push_reg(0x80000040, sp);   // push << t0 & xmethod >> to sp
+  push_reg(0x80000020, sp);   // push << t0 & xmethod >> to sp
   movptr_with_offset(t0, entry_point, offset);
   jalr(x1, t0, offset);
   if (retaddr != NULL) {
     bind(*retaddr);
   }
-  pop_reg(0x80000040, sp);   // pop << t0 & xmethod >> from sp
+  pop_reg(0x80000020, sp);   // pop << t0 & xmethod >> from sp
 }
 
 void MacroAssembler::call_VM_leaf(address entry_point, int number_of_arguments) {
