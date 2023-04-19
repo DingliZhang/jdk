ebdba13924134deb5397abfe09184d1c19c70440
Dingli Zhang <dingli@iscas.ac.cn>
2023-04-19 18:15:52 +0800

    Add castXtoX without reshape

--- src/hotspot/cpu/riscv/c2_MacroAssembler_riscv.cpp
+++ src/hotspot/cpu/riscv/c2_MacroAssembler_riscv.cpp
@@ -1775,3 +1775,79 @@ void C2_MacroAssembler::compare_floating_point_v(VectorRegister vd, BasicType bt
       ShouldNotReachHere();
   }
 }
+
+void C2_MacroAssembler::vector_integer_extend(VectorRegister dst, BasicType dst_bt,
+                                                  VectorRegister src, BasicType src_bt) {
+  assert(type2aelembytes(dst_bt) > type2aelembytes(src_bt) && type2aelembytes(dst_bt) <= 8 && type2aelembytes(src_bt) <= 4, "invalid element size");
+  assert(dst_bt != T_FLOAT && dst_bt != T_DOUBLE && src_bt != T_FLOAT && src_bt != T_DOUBLE, "should be integer element");
+  // https://github.com/riscv/riscv-v-spec/blob/master/v-spec.adoc#52-vector-operands
+  // The destination EEW is greater than the source EEW, the source EMUL is at least 1,
+  // and the overlap is in the highest-numbered part of the destination register group.
+  // Since LMUL=1, vd and vs cannot be the same.
+  assert_different_registers(dst, src);
+
+  Assembler::SEW sew = Assembler::elemtype_to_sew(dst_bt);
+  vsetvli(t0, x0, sew);
+  if (src_bt == T_BYTE) {
+    switch (dst_bt) {
+    case T_SHORT:
+      vsext_vf2(dst, src);
+      break;
+    case T_INT:
+      vsext_vf4(dst, src);
+      break;
+    case T_LONG:
+      vsext_vf8(dst, src);
+      break;
+    default:
+      ShouldNotReachHere();
+    }
+  } else if (src_bt == T_SHORT) {
+    if (dst_bt == T_INT) {
+      vsext_vf2(dst, src);
+    } else {
+      vsext_vf4(dst, src);
+    }
+  } else if (src_bt == T_INT) {
+    vsext_vf2(dst, src);
+  }
+}
+
+// Vector narrow from src to dst with specified element sizes.
+// High part of dst vector will be filled with zero.
+void C2_MacroAssembler::vector_integer_narrow(VectorRegister dst, BasicType dst_bt, int length_in_bytes,
+                                                  VectorRegister src, BasicType src_bt, VectorRegister tmp) {
+  assert(type2aelembytes(dst_bt) < type2aelembytes(src_bt) && type2aelembytes(dst_bt) <= 4 && type2aelembytes(src_bt) <= 8, "invalid element size");
+  assert(dst_bt != T_FLOAT && dst_bt != T_DOUBLE && src_bt != T_FLOAT && src_bt != T_DOUBLE, "should be integer element");
+  assert_different_registers(dst, tmp);
+  vmv1r_v(tmp, src);
+  clear_register_v(dst);
+  mv(t0, length_in_bytes);
+  if (src_bt == T_LONG) {
+    // https://github.com/riscv/riscv-v-spec/blob/master/v-spec.adoc#117-vector-narrowing-integer-right-shift-instructions
+    // Future extensions might add support for versions that narrow to a destination that is 1/4 the width of the source.
+    // So we can currently only scale down by 1/2 the width at a time.
+    vsetvli(t0, t0, Assembler::e32, Assembler::mf2);
+    vncvt_x_x_w(tmp, tmp);
+    if (dst_bt == T_SHORT || dst_bt == T_BYTE) {
+      vsetvli(t0, t0, Assembler::e16, Assembler::mf2);
+      vncvt_x_x_w(tmp, tmp);
+      if (dst_bt == T_BYTE) {
+        vsetvli(t0, t0, Assembler::e8, Assembler::mf2);
+        vncvt_x_x_w(tmp, tmp);
+      }
+    }
+  } else if (src_bt == T_INT) {
+      // T_SHORT
+      vsetvli(t0, t0, Assembler::e16, Assembler::mf2);
+      vncvt_x_x_w(tmp, tmp);
+      if (dst_bt == T_BYTE) {
+        vsetvli(t0, t0, Assembler::e8, Assembler::mf2);
+        vncvt_x_x_w(tmp, tmp);
+      }
+  } else if (src_bt == T_SHORT) {
+    vsetvli(t0, t0, Assembler::e8, Assembler::mf2);
+    vncvt_x_x_w(tmp, tmp);
+  }
+  vmv_v_v(dst, tmp);
+}
